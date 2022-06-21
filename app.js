cfcd1a953d8a7cb15eea7a6ed5a42b4b6d1d4d14
Bazylek12 <a.kosmowski7@gmail.com>
2022-06-21 21:00:39 +0200

    CRUD operations created

--- app.js
+++ app.js
@@ -3,14 +3,83 @@ const express = require('express');
 const status = require('http-status');
 const app = express();
 
-const { init } = require('./db');
+const { init, getAds, getAd, deleteAd, addAd, updateAd } = require('./db');
 
 app.use(express.json());
 
-// init().then(() => {
-    app.get('/heartbeat', (req, res) => {
-        res.send(new Date());
+init().then(() => {
+    app.get('/ads', async (req, res) => {
+        const ads = await getAds()
+        if (ads.length !== 0) {
+            res.send(ads);
+        }
+        res.statusCode = status.NO_CONTENT;
+        res.send();
+    });
+
+    app.get('/ads/:id', async (req, res) => {
+        const { id } = req.params;
+        const ad = await getAd(id);
+        if (ad) {
+            res.send(ad);
+        }
+        res.statusCode = status.NOT_FOUND;
+        res.send();
     })
-    app.listen(process.env.PORT, () => console.log('Server runnig'));
-// })
+
+    app.post('/ads', async (req, res) => {
+        const newAd = req.body;
+        const result = await addAd(newAd);
+        if (!newAd.title || !newAd.description || !newAd.author || !newAd.tags || !newAd.category || !newAd.price) {
+            res.statusCode = status.BAD_REQUEST;
+            res.send("Wrong data passed " + res.statusCode)
+        } else if (result.insertedCount === 1) {
+            res.statusCode = status.CREATED;
+            res.send("New ad created" + res.statusCode);
+        } else {
+            res.statusCode = status.INTERNAL_SERVER_ERROR;
+            res.send("Error: " + res.statusCode)
+        }
+    })
+
+    app.delete('/ads/:id', async (req, res) => {
+        const { id } = req.params;
+        const result = await deleteAd(id);
+        if (result.deletedCount === 1) {
+            res.statusCode = status.NO_CONTENT;
+            res.send("Ad deleted. " + res.statusCode);
+        } else {
+            res.statusCode = status.NOT_FOUND;
+            res.send("Error " + res.statusCode)
+        }
+    })
+
+    app.patch('/ads/:id', async (req, res) => {
+        const { id } = req.params;
+        const modified = req.body;
+        if (modified == null) {
+            res.statusCode = status.BAD_REQUEST;
+            res.send("Bad data. Error: " + res.statusCode);
+        } else {
+            const result = await updateAd(id, modified);
+            if (result.modifiedCount === 1) {
+                res.statusCode = status.ACCEPTED;
+                res.send("Ad updated. Code: " + res.statusCode);
+            } else if (result.matchedCount === 1) {
+                res.statusCode = status.CONFLICT;
+                res.send("Nothing to update. Make some changes. Error: " + res.statusCode);
+            } else {
+                res.statusCode = status.NOT_FOUND;
+                res.send("Ad doesn't exist. Error: " + res.statusCode);
+            }
+        }
+    });
+})
+
+    .finally(() => {
+        app.get('/heartbeat', (req, res) => {
+            res.send(new Date());
+        })
+        app.listen(process.env.PORT, () => console.log('Server runnig'));
+    })
 
--- db.js
+++ db.js
@@ -8,13 +8,50 @@ const AdsCollectionName = 'ads';
 let db;
 let adsCollection;
 
-const init = () => {
+const init = () => 
     MongoClient.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
         .then((client) => {
             db = client.db(process.env.MONGODB_DBNAME)
             adsCollection = db.collection(AdsCollectionName)
         })
         .catch(error => console.log(error));
-}
+    
+    const getAds = () => {
+        return adsCollection.find().toArray();
+    }
 
-module.exports = { init }
+    const getAd = (id) => {
+        return adsCollection.findOne({ _id: new ObjectId(id) });
+    }
+
+    const deleteAd = (id) => {
+        return adsCollection.deleteOne({ _id: new ObjectId(id) });
+    }
+
+    const addAd = (newAd) => {
+        let adToAdd = {};
+        adToAdd.title = newAd.title;
+        adToAdd.description = newAd.description;
+        adToAdd.author = newAd.author;
+        adToAdd.category = newAd.category;
+        adToAdd.tags = newAd.tags;
+        adToAdd.price = newAd.price;
+        adToAdd.createdTime = new Date().toString();
+        return adsCollection.insertOne(adToAdd);
+    }
+
+    const updateAd = (id, modified) => {
+        return adsCollection.updateOne
+        ({ _id: new ObjectId(id) },
+        { $set: {
+            "title": (modified.title),
+            "description": (modified.description),
+            "category": (modified.category),
+            "tags": (modified.tags),
+            "price": (modified.price),
+            }   
+        });
+    }
+
+
+module.exports = { init, getAds, getAd, deleteAd, addAd, updateAd }
